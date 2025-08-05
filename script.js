337d2f1a19ebe1106c870c97126bf43f4a822e62
Brihanna Torres <Brihanna1102@gmail.com>
2025-08-05 15:52:27 +0000

    added testimonial page

--- script.js
+++ script.js
@@ -1,40 +1,125 @@
-// console.log("Hello World");
+const reviews = [
+{
+    id: 1, 
+    name: 'Jeff Palmere',
+    job: 'Web Developer',
+    img: 'IMG_1137 (2).jpg',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
 
-// let myString = "a2b3c";
-// console.log(myString);
+},
+{
+    id: 2, 
+    name: 'Eve Abraha',
+    job: 'Teacher',
+    img: '#',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
 
-// let myNumVar = 123;
-// console.log(typeof myNumVar);
+},
+{
+    id: 3, 
+    name: 'Peter Jones',
+    job: 'Intern',
+    img: '#',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
 
-// console.log(10 + "eggs");
-// console.log(10 + 7 + "eggs");
-// console.log("eggs" + 10 + 7);
-// console.log(2==10);
+},
+];
+//select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = Document.getElementById('info');
 
-// //Both Conditions Need to be true
-// console.log(7>3)&&(5<10);
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-// //Only One condition must be true
-// console.log(10>3)|| (10>12);
+let currentItem = 0;
 
-// //If the statement is logical or not
-// console.log(!5>3);
+window.addEventListener('DOMContentLoaded', function(){
+   const item = reviews [currentItem];
+   img.src = item.img;
+   author.textContent = item.name;
+   job.textContent = item.job;
+   info.textContent = item.info;
 
-// function introduction (name, age) { 
-//     console.log("Hello, my name is " + name + "and I am" + age + "years old".)
-// }
+});
 
-// introduction("Brihanna", 105);
-
-const hour = new Date ().getHours();
-let greeting = "";
-
-if (hour<12){ 
-    greeting = "Good morning☀️";
-}else if (hour<18){
-    greeting = "Good afternoon";
-}else {
-    greeting = "Good evening";
+function showPerson(person){
+   const item = reviews[person];
+   img.src = item.img;
+   author.textContent = item.name;
+   job.textContent = item.job;
+   info.textContent = item.info;
 }
 
-document.getElementById("greeting").innerText = greeting;
+nextBtn.addEventListener( 'click', function (){
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+
+prevBtn.addEventListener( 'click', function (){
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log("Hello World");
+
+// // let myString = "a2b3c";
+// // console.log(myString);
+
+// // let myNumVar = 123;
+// // console.log(typeof myNumVar);
+
+// // console.log(10 + "eggs");
+// // console.log(10 + 7 + "eggs");
+// // console.log("eggs" + 10 + 7);
+// // console.log(2==10);
+
+// // //Both Conditions Need to be true
+// // console.log(7>3)&&(5<10);
+
+// // //Only One condition must be true
+// // console.log(10>3)|| (10>12);
+
+// // //If the statement is logical or not
+// // console.log(!5>3);
+
+// // function introduction (name, age) { 
+// //     console.log("Hello, my name is " + name + "and I am" + age + "years old".)
+// // }
+
+// // introduction("Brihanna", 105);
+
+// const hour = new Date ().getHours();
+// let greeting = "";
+
+// if (hour<12){ 
+//     greeting = "Good Morning";
+// }else if (hour<18){
+//     greeting = "Good Afternoon";
+// }else {
+//     greeting = "Good Evening";
+// }
+
+// document.getElementById("greeting").innerText = greeting;
