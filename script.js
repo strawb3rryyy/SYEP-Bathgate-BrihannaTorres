5dfed4b0de345412086f15c96826489e8289a0b1
Brihanna Torres <Brihanna1102@gmail.com>
2025-08-07 23:44:39 +0000

    improved testimonials

--- script.js
+++ script.js
@@ -1,73 +1,78 @@
 const reviews = [
 {
-    id: 1, 
-    name: 'Jeff Palmere',
-    job: 'Web Developer',
-    img: 'IMG_1137 (2).jpg',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
-
+id: 1,
+name: 'susan smith',
+job: 'web developer',
+img: 'https://www.course-api.com/images/people/person-1.jpeg',
+text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?",
 },
 {
-    id: 2, 
-    name: 'Eve Abraha',
-    job: 'Teacher',
-    img: '#',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
-
+id: 2,
+name: 'anna johnson',
+job: 'web designer',
+img: 'https://www.course-api.com/images/people/person-2.jpeg',
+text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
 },
 {
-    id: 3, 
-    name: 'Peter Jones',
-    job: 'Intern',
-    img: '#',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
-
+id: 3,
+name: 'peter jones',
+job: 'intern',
+img: 'https://www.course-api.com/images/people/person-4.jpeg',
+text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
 },
 ];
-//select items
+// select items
 const img = document.getElementById('person-img');
 const author = document.getElementById('author');
 const job = document.getElementById('job');
-const info = Document.getElementById('info');
+const info = document.getElementById('info');
 
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
 
+// set starting item
 let currentItem = 0;
 
-window.addEventListener('DOMContentLoaded', function(){
-   const item = reviews [currentItem];
-   img.src = item.img;
-   author.textContent = item.name;
-   job.textContent = item.job;
-   info.textContent = item.info;
-
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+const item = reviews[currentItem];
+img.src = item.img;
+author.textContent = item.name;
+job.textContent = item.job;
+info.textContent = item.text;
 });
 
-function showPerson(person){
-   const item = reviews[person];
-   img.src = item.img;
-   author.textContent = item.name;
-   job.textContent = item.job;
-   info.textContent = item.info;
+// show person based on item
+function showPerson(person) {
+const item = reviews[person];
+img.src = item.img;
+author.textContent = item.name;
+job.textContent = item.job;
+info.textContent = item.text;
 }
-
-nextBtn.addEventListener( 'click', function (){
-    currentItem++;
-    if (currentItem > reviews.length - 1) {
-        currentItem = 0;
-    }
-    showPerson(currentItem);
+// show next person
+nextBtn.addEventListener('click', function () {
+currentItem++;
+if (currentItem > reviews.length - 1) {
+currentItem = 0;
+}
+showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener('click', function () {
+currentItem--;
+if (currentItem < 0) {
+currentItem = reviews.length - 1;
+}
+showPerson(currentItem);
 });
 
 
-prevBtn.addEventListener( 'click', function (){
-    currentItem--;
-    if (currentItem < 0) {
-        currentItem = reviews.length - 1;
-    }
-    showPerson(currentItem);
-});
+
+
+
+
 
 
 
